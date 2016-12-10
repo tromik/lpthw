3ac63c450ab97445546dc274f805a76bbf0c4f25
tromik <tromik@gmail.com>
2016-12-10 18:52:43 -0500

    Exercises 1 to 4

---
+++ ex1b.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Printing! Yay.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+# print "This is an extra line."
